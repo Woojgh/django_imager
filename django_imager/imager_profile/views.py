5800ab4ae91da88272c75754498290f7ae083277
bonanashelby <bonanashelby@gmail.com>
2017-06-21 15:08:03 -0700

    worked on removing some extra files and adding a coveragerc, tests and some changes to the views

--- django_imager/imager_profile/views.py
+++ django_imager/imager_profile/views.py
@@ -8,46 +8,9 @@ from django.shortcuts import render, redirect
 
 def home_view(request):
     """Home view callable, for the home page."""
-    # template = loader.get_template('django_imager/home.html')
-    # response_body = template.render()
-    # return HttpResponse(response_body)
-    context = {
-        'food': 'steak'
-    }
+    context = {'food': 'steak'}
     return render(request, 'django_imager/home.html', context=context)
-
-
-# def login_view(request):
-#     return render(request, 'django_imager/login.html')
 
 
 def account_view(request):
     return render(request, 'django_imager/account.html')
-
-
-@csrf_exempt
-def register_user(request):
-    context = {}
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            pass
-    else:
-        form = RegisterForm(auto_id=False)
-    context['form'] = form
-    return render_to_response('templates/register.htm', context, context_instance=RequestContext(request))
-
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
--- django_imager/imgr_pfile/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImgrPfileConfig(AppConfig):
-    name = 'imgr_pfile'
