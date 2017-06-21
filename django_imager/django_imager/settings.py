4704db0a47887de08f3862298aec029870108c86
Woojgh <jamessalamonsen@gmail.com>
2017-06-20 22:28:54 -0700

    wondering where accounts went?

--- django_imager/django_imager/settings.py
+++ django_imager/django_imager/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/accounts/profile'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
--- django_imager/django_imager/urls.py
+++ django_imager/django_imager/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     url(r'^signup/$', signup, name='signup'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^login/$', auth_views.login, {'template_name': 'django_imager/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/accounts/profile'}, name='logout'),
     url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
