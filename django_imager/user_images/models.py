500d935f0b7e3c5b951d12636cdb1f42db62eae3
Woojgh <jamessalamonsen@gmail.com>
2017-06-24 09:36:03 -0700

    fixing Images

--- django_imager/user_images/models.py
+++ django_imager/user_images/models.py
@@ -4,6 +4,6 @@ import datetime
 
 class Images(models.Model):
     image_upload = models.ImageField(upload_to='images', default='no.jpg')
-    date_added = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    # date_added = models.DateTimeField(auto_now_add=True)
+    # title = models.CharField(max_length=255)
+    # author = models.CharField(max_length=255)
