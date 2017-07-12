024b3b205e52b0f3686be205f841e727d26d8ec1
bonanashelby <bonanashelby@gmail.com>
2017-07-12 12:56:49 -0700

    still working on errors.

--- django_imager/imager_profile/views.py
+++ django_imager/imager_profile/views.py
@@ -3,12 +3,8 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth import logout
 from user_images.models import Photo, Album, Item, AddImage
 from django.core.cache import cache
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from user_images.models import ImageUploadForm
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.core.urlresolvers import reverse_lazy
-from imager_profile.forms import DocumentForm
 
 
 def home_view(request):
@@ -36,7 +32,7 @@ def image_view(request):
     context = {
         "photos": photos,
         "albums": albums,
-        }
+    }
     # import pdb; pdb.set_trace()
     return render(request, 'user_images/user_images.html', context=context)
 
@@ -57,11 +53,9 @@ def add_image_view(request):
     return render(request, "user_images/add_image.html", {"form": form})
 
 
-
-
 def thumb_view(request):
     items = Item.objects.all()
     context = {
         "items": items,
-        }
+    }
     return render(request, 'user_images/thumb.html', context=context)
