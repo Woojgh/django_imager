e9081208553cbf011a16583ca061cff15749e8f8
Woojgh <jamessalamonsen@gmail.com>
2017-07-19 19:23:07 -0700

    adding serializers

--- django_imager/imager_api/serializers.py
+++ django_imager/imager_api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from imager_api.models import Api, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
 
 class ApiSerializer(serializers.ModelSerializer):
     class Meta:
         model = Api
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
